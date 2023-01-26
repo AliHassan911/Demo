00303c8182daa1585141b1187e93c23c240e7af4
HasSanK911 <kaleemikennels@gmail.com>
2023-01-26 16:04:41 +0500

    scvnkjads

---
+++ src/app/studs/studs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studs',
+  templateUrl: './studs.component.html',
+  styleUrls: ['./studs.component.css']
+})
+export class StudsComponent {
+
+}
